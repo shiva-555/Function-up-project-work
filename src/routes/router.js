e2652c5462ac622ac0d3d8b5535967ae9afab506
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-07-08 17:19:38 +0530

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,19 +1,23 @@
 const express=require("express")
 const router=express.Router()
-// const moment = require("moment")
 const userController=require("../controller/userController")
 const bookController=require("../controller/bookController")
 const reviewController=require("../controller/reviewController")
 
-
+/******************************[USER API]**************************************************/
 router.post("/register",userController.createUser)
 router.post("/login",userController.createLogin)
+
+/******************************[BOOK API]**************************************************/
 router.post("/books",bookController.createBook)
 router.get("/books",bookController.getBook)
-// router.get("/books/:bookId", bookController.getBookById)
+router.get("/books/:bookId", bookController.getBookById)
 router.put("/books/:bookId", bookController.updateBookById)
 router.delete("/books/:bookId", bookController.deletedBook)
+
+/******************************[REVIEW API]**************************************************/
 router.post("/books/:bookId/review", reviewController.createReview)
+router.put("/books/:bookId/review/:reviewId",reviewController.createReview)
 
 
 module.exports=router
