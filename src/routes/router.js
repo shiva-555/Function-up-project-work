6c981a34bdbe18466c8fb9a2178ca1014f81fdb5
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2022-07-09 18:03:38 +0530

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,23 +1,26 @@
 const express=require("express")
 const router=express.Router()
+// const moment = require("moment")
 const userController=require("../controller/userController")
 const bookController=require("../controller/bookController")
 const reviewController=require("../controller/reviewController")
+const mw =require("../middleware/middlewareController")
 
-/******************************[USER API]**************************************************/
+
+
+
 router.post("/register",userController.createUser)
 router.post("/login",userController.createLogin)
+router.post("/books", mw.authentication,mw.authorization, bookController.createBook)
+router.get("/books", mw.authentication ,bookController.getBook)
+// router.get("/books/:bookId", bookController.getBookById)
+router.put("/books/:bookId",mw.authentication,mw.authorization, bookController.updateBookById)
+router.delete("/books/:bookId",mw.authentication, mw.authorization, bookController.deletedBook)
 
-/******************************[BOOK API]**************************************************/
-router.post("/books",bookController.createBook)
-router.get("/books",bookController.getBook)
-router.get("/books/:bookId", bookController.getBookById)
-router.put("/books/:bookId", bookController.updateBookById)
-router.delete("/books/:bookId", bookController.deletedBook)
+// ************REVIWS API*******************************
 
-/******************************[REVIEW API]**************************************************/
-router.post("/books/:bookId/review", reviewController.createReview)
-router.put("/books/:bookId/review/:reviewId",reviewController.createReview)
-
+router.post("/books/:bookId/review",mw.authentication, reviewController.createReview)
+router.put("/books/:bookId/review/:reviewId", mw.authentication,mw.authorization,reviewController.updateReview)
+router.delete("/books/:bookId/review/:reviewId",mw.authentication, mw.authorization, reviewController.deletedReview)
 
 module.exports=router
